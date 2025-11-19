062229a5cbb3a47d6d3972404524984fd8b2a727
andresfalcon20 <andresfalcon.18@hotmail.com>
2025-11-19 15:42:27 -0500

    ola

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import '../styles/App.css'
+import '../styles/App.css';
 
 const Home = () => {
     return (
         <>
-            <div className="min-h-screen flex flex-col lg:flex-row bg-gradient-to-br from-sky-100 via-indigo-200 to-indigo-400">
+            <div className="flex flex-col lg:flex-row items-center justify-center bg-gradient-to-br from-sky-100 via-indigo-200 to-indigo-400">
 
-                {/* Columna izquierda (40%) */}
-                <div className="lg:w-2/5 w-full flex items-center justify-center p-20 bg-transparent">
+                {/* Columna izquierda (logo) */}
+                <div className="lg:w-2/5 w-full flex items-center justify-center px-10 py-10 bg-transparent">
                     <img
                         src="../src/assets/lgo.png"
                         alt="Logo INAMHI"
@@ -14,9 +14,9 @@ const Home = () => {
                     />
                 </div>
 
-                {/* Columna derecha más grande (60%), centrada */}
+                {/* Columna derecha */}
                 <div className="lg:w-3/5 w-full flex flex-col justify-center items-center px-8 py-16">
-                    <div className="max-w text-center ml-20">
+                    <div className="text-center max-w-xl">
                         <h1 className="text-5xl font-bold text-blue-700 leading-tight mb-6 drop-shadow-lg">
                             Bienvenido al Sistema de Soporte Técnico INAMHI
                         </h1>
@@ -25,16 +25,18 @@ const Home = () => {
                             Nuestro equipo trabaja con dedicación para ofrecer soluciones confiables, ágiles y de calidad.
                         </p>
 
-                        <a href="/formulario" className="bg-white text-indigo-700 font-semibold py-3 px-8 rounded-full shadow-md hover:bg-indigo-100 transition duration-300">
+                        <a
+                            href="/formulario"
+                            className="bg-white text-indigo-700 font-semibold py-3 px-8 rounded-full shadow-md hover:bg-indigo-100 transition duration-300"
+                        >
                             INGRESAR
                         </a>
                     </div>
                 </div>
 
             </div>
-
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
