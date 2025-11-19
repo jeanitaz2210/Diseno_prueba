8b08d999d7fa70d1e5e1bf9112a13d9455bb4fd8
jeanitaz <jeanzeykoharcode@gmail.com>
2025-11-19 12:33:38 -0500

    prueba 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/styles/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="body">
+      <section className="section ">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between items-center flex-col lg:flex-row md:mt-20">
+                <div className="w-full lg:w-1/2">
+                    <h2
+                        className="font-manrope text-5xl text-white font-bold leading-[4rem] mb-7 text-center lg:text-left">
+                      Bienvenido al Sistema de Soporte Técnico INAMHI</h2>
+                    <p className="text-lg text-gray-500 mb-16 text-center lg:text-left">These people work on making our
+                        product best.</p>
+                    <button className="cursor-pointer py-3 px-8 w-60 bg-indigo-600 text-white text-base font-semibold transition-all duration-500 block text-center rounded-full hover:bg-indigo-700 mx-auto lg:mx-0">Join
+                        our team</button>
+                </div>
+                <div className="w-full lg:w-1/2 lg:mt-0 md:mt-40 mt-16 max-lg:max-w-2xl">
+                    <div className="grid grid-cols-1 min-[450px]:grid-cols-2 md:grid-cols-3 gap-8">
+                        <img src="../src/assets/Adm.jpg" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover md:mt-20 mx-auto min-[450px]:mr-0" />
+                        <img src="../src/assets/arquitectura.jpg" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover mx-auto min-[450px]:ml-0 md:mx-auto" />
+                        <img src="../src/assets/mantenimiento.jpg" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover md:mt-20 mx-auto min-[450px]:mr-0 md:ml-0" />
+                        <img src="../src/assets/software.jpg" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover mx-auto min-[450px]:ml-0 md:mr-0 md:ml-auto" />
+                        <img src="../src/assets/3redes.webp" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover md:-mt-20 mx-auto min-[450px]:mr-0 md:mx-auto" />
+                        <img src="../src/assets/images.jpg" alt="Team tailwind section"
+                            className="w-44 h-56 rounded-2xl object-cover mx-auto min-[450px]:ml-0 md:mr-0" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+                                            
+    </div>
     </>
   )
 }
-
-export default App
